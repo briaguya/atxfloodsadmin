fa0a21330a36c72b230eb7d2e190bc710e47c03d
Brian Smith <briaguya@gmail.com>
2017-05-02 12:24:29 -0500

    Getting going with the react-cogito example

---
+++ src/ConfirmForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+class ConfirmForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: props.error,
+      verificationCode: '',
+    };
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.verificationCode);
+  }
+
+  changeVerificationCode = (event) => {
+    this.setState({ verificationCode: event.target.value });
+  }
+
+  render = () => (
+    <form onSubmit={this.onSubmit}>
+      <div>{this.props.error}</div>
+      <label>
+        Verification Code
+        <input placeholder="code" onChange={this.changeVerificationCode} required />
+      </label>
+      <button type="submit">Submit</button>
+      <button type="button" onClick={this.props.onResend}>Resend code</button>
+      <button type="button" onClick={this.props.onCancel}>Cancel</button>
+
+    </form>
+  )
+}
+ConfirmForm.propTypes = {
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func,
+  onResend: PropTypes.func,
+  error: PropTypes.string,
+};
+
+export default ConfirmForm;
